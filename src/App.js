55db9227d0b2d8edb72aaa44bf200623ef5f7734
thenoxwitch <131623738+thenoxwitch@users.noreply.github.com>
2023-10-02 15:13:08 +0100

    Added css to app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
 import "./App.css";
-import Weather from "./Weather";
-import { Audio } from "react-loader-spinner";
+import "./styles.css";
+import Search from "./Search";
+import "./Search.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Weather city="Mexico" />
+      <nav class="navbar navbar-expand-lg bg-light navbar">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="index.html">
+            {" "}
+            WeatherPixel
+          </a>
+          <img
+            src="https://cdn-icons-png.flaticon.com/128/365/365865.png"
+            width="25px"
+            alt="cute-icon"
+          />
+        </div>
+      </nav>
 
-        <Audio
-          height="250"
-          width="250"
-          radius="9"
-          color="plum"
-          ariaLabel="loading"
-          wrapperStyle
-          wrapperClass
+      <div class="container-sm">
+        <br />
+        <img
+          class="rainbow-gif"
+          src="https://media.giphy.com/media/huyIAhSLfOeCk/giphy.gif"
+          width="50px"
+          alt="cute"
         />
-      </header>
+        <br />
+
+        <Search />
+
+        <br />
+        <div class="weather-forecast" id="forecast"></div>
+      </div>
+      <br />
+      <p class="bottom-link">
+        💜
+        <a
+          href="https://github.com/thenoxwitch/weather-app-shecodes-project"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Open Source Code
+        </a>
+        by Atenas G I.💜
+      </p>
+      <script
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz"
+        crossorigin="anonymous"
+      ></script>
     </div>
   );
 }
-
-export default App;
