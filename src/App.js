d2c030d5f31ab0336c7b600e7a6f54c64b675843
thenoxwitch <131623738+thenoxwitch@users.noreply.github.com>
2023-10-02 13:48:43 +0100

    Displayed weather by adding axios integration

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Weather from "./Weather";
+import { Audio } from "react-loader-spinner";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Weather city="Mexico" />
+
+        <Audio
+          height="80"
+          width="80"
+          radius="9"
+          color="plum"
+          ariaLabel="loading"
+          wrapperStyle
+          wrapperClass
+        />
       </header>
     </div>
   );
