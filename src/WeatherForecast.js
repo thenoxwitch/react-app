cc97feffa99e0e408a4f92c57c5a93fdb9bcb39c
thenoxwitch <131623738+thenoxwitch@users.noreply.github.com>
2023-10-20 17:39:20 +0100

    debugged site

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -21,12 +21,15 @@ if (loaded) {
        <div className="WeatherForecast d-inline-flex p-2">
          <div className="row">
            {forecast.map(function (dailyForecast, index) {
-             if (index < 5)
+             if (index < 5) {
                return (
                  <div className="col" key={index}>
                    <WeatherForecastDay data={dailyForecast} />
                  </div>
                );
+             } else {
+              return null;
+             }
            })}
          </div>
        </div>
